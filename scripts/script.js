8b19e489eb6d80e4db086a044759e21135775892
Evgenyii <milyavichus@yandex.ru>
2024-02-14 18:15:29 +0200

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,48 @@
+let menu__btn = document.querySelector(".menu__btn");
+let hamburger__content = document.querySelector(".hamburger__content");
+// const menu = document.querySelector(".menu").cloneNode(1);
+// const body = document.body;
 
-const icon = document.querySelector(".n__c__m__img1");
-const hanb = document.querySelector(".hamburger__content");
-const body = document.body;
+menu__btn.addEventListener("click", hambHandler);
 
-// При клике на иконку hamb вызываем ф-ию hambHandler
-icon.addEventListener("click", hambHandler);
-
-// Выполняем действия при клике ..
 function hambHandler(e) {
   e.preventDefault();
-  // Переключаем стили элементов при клике
-  hanb.classList.toggle("open");
-  icon.classList.toggle("active");
-  body.classList.toggle("noscroll");
-  renderPopup();
+  menu__btn.classList.toggle("active");
+  hamburger__content.classList.toggle("active");
+  // body.classList.toggle("noscroll");
+  // renderPopup();
 }
 
-// Здесь мы рендерим элементы в наш попап
-function renderPopup() {
-  hanb.appendChild(menu);
-}
+// function renderPopup() {
+//   popup.appendChild(menu);
+// }
 
-// Код для закрытия меню при нажатии на ссылку
-const links = Array.from(menu.children);
+// // Код для закрытия меню при нажатии на ссылку
 
-// Для каждого элемента меню при клике вызываем ф-ию
-links.forEach((link) => {
-  link.addEventListener("click", closeOnClick);
-});
+// const links = Array.from(menu.children);
 
-// Закрытие попапа при клике на меню
-function closeOnClick() {
-  hanb.classList.remove("open");
-  icon.classList.remove("active");
-  body.classList.remove("noscroll");
-}
+// links.forEach((link) => {
+//   link.addEventListener("click", closeOnClick);
+// });
+
+// function closeOnClick() {
+//   popup.classList.remove("open");
+//   hamb.classList.remove("active");
+//   body.classList.remove("noscroll");
+// }
+
+
+// // Код для закрытия меню при нажатии на ссылку
+// const links = Array.from(menu.children);
+
+// // Для каждого элемента меню при клике вызываем ф-ию
+// links.forEach((link) => {
+//   link.addEventListener("click", closeOnClick);
+// });
+
+// // Закрытие попапа при клике на меню
+// function closeOnClick() {
+//   hanb.classList.remove("open");
+//   icon.classList.remove("active");
+//   body.classList.remove("noscroll");
+// }
